6ceeb0991fefb8f3f067632cabbbdce9f95ac87f
edu20056 <ejcanessaq@gmail.com>
2025-05-09 18:30:45 -0600

    Added all information to Information class which will be used for the information display.

--- Sources/UIElements/InformationScreen/InformationScreen.hpp
+++ Sources/UIElements/InformationScreen/InformationScreen.hpp
@@ -6,8 +6,16 @@
 #include "raylib.h"
 
 struct Information {
-    int generations;
-    std::vector<std::array<int, 2>> diedInWaves; // list of arrays that contains 2 ints. First int is wave number and second int is enemies that diesd
+    int waveNum = 1; // Same as generations, also we now enemies killed per round is 5 + round, 
+
+    // Genetic Alg info
+    int mutaciones;
+    int prob = 65;
+
+    // Fits of enemies
+    std::vector<int> fits = {0,0,0,0};
+
+    // Towers info
     std::array<std::array<int, 4>, 3> towers;
 
 
