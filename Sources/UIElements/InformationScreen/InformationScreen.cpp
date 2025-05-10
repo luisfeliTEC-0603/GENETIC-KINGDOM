4146c2f319e6f8247eabddf1b8725fb50195dc3d
edu20056 <ejcanessaq@gmail.com>
2025-05-09 18:53:21 -0600

    Added all game information except enemies fit.

--- Sources/UIElements/InformationScreen/InformationScreen.cpp
+++ Sources/UIElements/InformationScreen/InformationScreen.cpp
@@ -4,58 +4,69 @@
 
 void ShowInformationScreen(Information& info) {
     int CELL_SIZE = 16;
-    int spacing = 3 * CELL_SIZE; 
+    int spacing = 3 * CELL_SIZE;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        // Title 
-        DrawText("Towers:", 50, 50, 20, RAYWHITE);
+        // General Game Info
+        DrawText(TextFormat("Numero de Oleada: %d", info.waveNum), 50, 30, 20, RAYWHITE);
+        DrawText(TextFormat("Probabilidad: %d%%", info.prob), 50, 60, 20, RAYWHITE);
+        DrawText(TextFormat("Mutaciones: %d", info.mutaciones), 50, 90, 20, RAYWHITE);
 
-        int startX = 4 * CELL_SIZE; // Start at x
-        int startY = 5 * CELL_SIZE; // Start at y
+        // Title
+        DrawText("Towers:", 50, 140, 20, RAYWHITE);
 
-        // Names of towers depending on number
         const char* towerNames[3] = { "Archer", "Wizard", "Artillery" };
 
-        // Display towers
-        for (int i = 0; i < 3; ++i) {  // 3 because there are 3 types of towers
-            float xPos = startX; // constant while deploying towrs since it is vertical.
-            float yPos = startY + i * (7 * CELL_SIZE + spacing); // Adjust y tower posicion
+        int startX = 4 * CELL_SIZE;
+        int startY = 10 * CELL_SIZE;
 
-            // Show type (Archer, Wizard, Artillery)
+        for (int i = 0; i < 3; ++i) {
+            float xPos = startX + i * (7 * CELL_SIZE + spacing);
+            float yPos = startY;
+
             char typeText[32];
             sprintf(typeText, "%s", towerNames[i]);
             int typeTextWidth = MeasureText(typeText, 16);
-            DrawText(typeText, xPos + (CELL_SIZE / 2) - (typeTextWidth / 2), yPos, 16, RAYWHITE);
+            DrawText(typeText, xPos + (CELL_SIZE * 2) - (typeTextWidth / 2), yPos, 16, RAYWHITE);
 
-            // Shpow levels (Level 1, Level 2, Level 3)
-            yPos += CELL_SIZE + 5; // Move down for levels (below type)
+            yPos += CELL_SIZE + 5;
 
             for (int level = 1; level <= 3; ++level) {
                 char levelText[32];
                 sprintf(levelText, "Level %d", level);
                 int levelTextWidth = MeasureText(levelText, 16);
+                DrawText(levelText, xPos + (CELL_SIZE * 2) - (levelTextWidth / 2), yPos, 16, RAYWHITE);
 
-                // Draw level in corresponding position
-                DrawText(levelText, xPos + (CELL_SIZE / 2) - (levelTextWidth / 2), yPos, 16, RAYWHITE);
-
-                // Show the quantities of each level
                 char amountText[32];
                 sprintf(amountText, "%d", info.towers[i][level]);
                 int amountTextWidth = MeasureText(amountText, 16);
+                DrawText(amountText, xPos + (CELL_SIZE * 2) - (amountTextWidth / 2), yPos + CELL_SIZE + 5, 16, RAYWHITE);
 
-                // Draw the amounts under each level
-                DrawText(amountText, xPos + (CELL_SIZE / 2) - (amountTextWidth / 2), yPos + CELL_SIZE + 5, 16, RAYWHITE);
+                yPos += CELL_SIZE + 18;
+            }
+        }
 
-                // Move the position to the next level
-                yPos += CELL_SIZE + 18; // Adjust the spacing between levels
+        int tableX = 400;  // <- ANTES estaba más lejos, lo movemos a la izquierda
+
+        DrawText("Oleada:", tableX, 30, 20, RAYWHITE);
+        DrawText("Muertes:", tableX, 60, 20, RAYWHITE);
+        
+        if (info.waveNum > 1) {
+            int offset = 100; // espacio desde texto hasta primer número
+        
+            for (int i = 1; i < info.waveNum; ++i) {
+                int xPos = tableX + offset + (i - 1) * 50;  // espacio entre números
+        
+                DrawText(TextFormat("%d", i), xPos, 30, 20, RAYWHITE);        // número de oleada
+                DrawText(TextFormat("%d", 5 + i), xPos, 60, 20, RAYWHITE);    // muertes
             }
         }
 
         if (IsKeyPressed(KEY_N)) {
-            break; // Close with N
+            break;
         }
 
         EndDrawing();
